642102ff2dedd64bc265081392cf957f376af6fc
yaroslavkrutiak <yaroslav.krut02@gmail.com>
2021-05-16 01:18:51 +0300

    Done 1/2

--- index.ts
+++ index.ts
@@ -1,8 +1,39 @@
-// @ts-ignore
-const fetch = require('node-fetch')
-http('https://cat-fact.herokuapp.com/facts').then(a=>console.log(a)).catch(e=>console.error(e))
+getResponse<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts"
+).then(async content => {
+    const article = document.querySelector("article");
+    article.innerHTML = await setHtml(content);
+});
 
-async function http(url: string): Promise<any>{
-    const res = await fetch(url);
-    return await res.json()
+interface Post {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+async function getResponse<T>(
+    url: string
+): Promise<T> {
+    let body, response;
+    try {
+        response = await fetch(url);
+        body = await response.json();
+    } catch (e) {
+        console.error(e);
+    }
+    return body;
+}
+
+async function setHtml(
+    content: Post[]
+): Promise<string> {
+    let html = ``;
+    content.forEach(elem=>html+=`    
+    <ul>
+        <li><span class="postId">Post ID: ${elem.id}</span>      <span class="userId">User ID: ${elem.userId}</span></li>
+        <li><span class="postTitle">${elem.title}</span></li>
+        <li><span class="postBody">${elem.body}</span></li>
+    </ul>`)
+    return html;
 }
